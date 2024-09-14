0cbf94e9ae54139b62ee853df3ee8f93e4dac328
Pablo Paradela <pablo.paradela@appliedtechnology.se>
2024-09-14 19:50:33 +0200

    all exercises passed

--- src/main/java/ThreeSmallMethods.java
+++ src/main/java/ThreeSmallMethods.java
@@ -3,7 +3,6 @@ import java.util.Arrays;
 public class ThreeSmallMethods {
 
     public boolean isYoung (int age) {
-
         if(age <= 30){
             return true;
         }
@@ -11,11 +10,7 @@ public class ThreeSmallMethods {
             return false;
         }
     }
-
     public boolean hasLegalGenderManInSweden(String personalNumber){
-
-
-
         if (personalNumber.length() == 10){
             if(personalNumber.charAt(8) % 2 != 0){
                 return true;
@@ -28,19 +23,12 @@ public class ThreeSmallMethods {
         return false;
         }
     }
-
-
     public int firstTwoDigitInteger(int[] numbers) {
-
-
-
-
         for(int pair : numbers){
             if( pair>9 && pair<=99 || pair<-9 && pair>=-99){
                 return pair;
             }
         }
-
         return -1;
     }
-    }
+}
